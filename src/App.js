8f2e28102306d2edd88da44c36366cb21604cdb9
mithilesh2023 <mithilesht2023@gmail.com>
2023-02-07 01:22:15 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,40 +1,83 @@
+import { useState } from 'react';
 import './App.css';
-import React, { useState } from 'react';
-import Footer from "./components/Footer";
-import Input from "./components/Input";
 import Box from './components/Box';
-function App() {
-  const[todo,setTodo]=useState([]);
+import uuid from 'react-uuid'
 
-const removeAdd=(id)=>{
-    const newData = todo.filter(
-      (id,index)=>{
-        if (index !==id){
-          return true;
-        }else{
-          return false;
-        }
-    })
-    setTodo(newData)
-}
- const addtodo = (item)=>{
-    // console.log(item);
-    setTodo([
-      ...todo,
-      {
-        item,time:new Date().toLocaleTimeString()
-      }
-    ])
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  const [item, setItem] = useState("");
+  const [itemList, setItemList] = useState([]);
+  const[toggleBtn,setToggleBtn]=useState(false)
+  const[itemId,setItemId]=useState()
+
+  const changeHandler = (e) => {
+    setItem(e.target.value)
   }
-  console.log(todo);
+  const addItem = () => {
+    if(toggleBtn){
+      const newList=itemList.map((todo)=>{
+        if(todo.id===itemId){
+          return {...todo,itemName:item}
+        }
+        return todo;
+      })
+       setItemList(newList)
+       setToggleBtn(false)
+       setItem("")
+       setItemId()
+      toast.info("Item updated successfully");
+
+    }else{
+      const itemObj = { id: uuid(), itemName: item }
+      setItemList((prevItem) => [...prevItem, itemObj])
+      setItem("");
+      toast.success("Item added successfully");
+    }
+  }
+  const deleteItem=(id)=>{
+    // console.log(e)
+    const filterItem=itemList.filter((value)=>{
+      return value.id !== id
+    })
+    setItemList(filterItem)
+    toast.error("Item deleted successfully");
+
+  }
+  const deleteAll=()=>{
+    setItemList([])
+    toast.error("All items deleted");
+
+  }
+  const editBtn=(id)=>{
+    const addTodo=itemList.find((todo)=>{
+      return todo.id===id;
+    })
+    // console.log('id',id)
+    setItem(addTodo.itemName)
+    setToggleBtn(true)
+    setItemId(id)
+    // setItem("")
+  }
   return (
-    <div className='App'>
-    <div className="bg-gray-600 border-2 w-2/5 border-gray-700 rounded mt-2 border-solid mx-auto">
-        <h1 className="text-white text-4xl p-3">Todo List</h1>
-        <Input add={addtodo}/>
-        <Box data={todo} removeTodo={removeAdd}/>
-        <Footer/>
-   </div>
+    <div className='bg-slate-700 h-screen'>
+
+      <div className=' w-[55%] py-1  mx-auto rounded-lg bg-black'>
+        <div className='flex gap-2 py-3 px-2 w-full mx-auto'>
+          <input className='w-[90%] border px-2 rounded shadow-lg text-2xl' type="text" placeholder='Add Todo...' value={item} onChange={changeHandler}></input>
+
+          <button className=' rounded-lg py-2 px-3 w-[20%]   bg-green-500 shadow-lg text-xl  text-white justify-center items-center' onClick={addItem} disabled={item.length <=2 ? true : false} 
+           >{toggleBtn?"update":"add item"}<ToastContainer theme="colored"/></button>
+
+          <button className='w-[20%] rounded-lg py-3 px-2 bg-red-500 shadow-lg text-xl text-white items-center' onClick={()=>deleteAll()}>Del All</button>
+
+        </div>
+        <div>
+        <Box itemList={itemList} deleteItem={deleteItem} editBtn={editBtn} />
+      </div>
+      </div>
+     
     </div>
   );
 }
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,18 +1,34 @@
-import React from "react";
-import Item from "./Item";
-const Box=(props)=>{
-    const items = props.data.map(
-       
-        (singleData,index) => {
-            return<Item removeTodo={props.removeTodo} key={index} id={index}
-             item={singleData.item} time={singleData.time}/>
-        }
-    )
-    return(
-      <>
-        {items}
-        </>
-       
+import React from 'react'
+import { AiFillDelete } from 'react-icons/ai'
+import { BiEdit } from 'react-icons/bi'
+const Box = ({ itemList, deleteItem, editBtn }) => {
+    // const abc=""
+    // const {itemList}=props;
+    return (
+        <div className=' w-[90%]  mx-auto  rounded-lg '>
+            <div className=' p-0'>
+
+                {itemList?.length ?
+                    itemList.map((val) => {
+                        return (
+                            <>
+                                <ul className='px-1 py-2 mb-1 flex gap-2 mx-3' key={val.id} >
+                                    <li className='w-[79%] p-3 text-2xl border rounded shadow-lg text-white' >{val.itemName}</li>
+
+                                    <button className='w-[9%] rounded-lg px-3 text-3xl bg-green-500 shadow-lg  text-white items-center justify-center' onClick={() => editBtn(val.id)} ><BiEdit /></button>
+
+                                    <button className='w-[9%] rounded-lg px-3 text-3xl bg-red-500 shadow-lg  text-white items-center justify-center' onClick={() => deleteItem(val.id)}><AiFillDelete /></button>
+
+                                </ul>
+
+                            </>
+                        )
+                    }) : <div className='w-full p-3 text-3xl mb-2 bg-black text-white border rounded'><span>No item to display...</span></div>
+                }
+            </div>
+
+        </div >
     )
 }
+
 export default Box
